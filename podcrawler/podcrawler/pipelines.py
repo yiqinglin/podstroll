409f1bf2d0406770270b9d6c3b0ea0f61a1d04c0
Yiqing <yiqinglin.me@gmail.com>
2019-03-05 14:51:27 -0800

    feat: podcrawler for podcast index.

---
+++ podcrawler/podcrawler/pipelines.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+# import json
+import pymongo
+
+class MongoPipeline(object):
+    collection_name = 'podcasts'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def process_item(self, item, spider):
+        collection = self.db[self.collection_name]
+
+        # Check if item already exists in the DB:
+        # exising_item = collection.find_one({ 'podcast_id': item['podcast_id'] })
+        # If not, save it to DB
+        if item.get('author'):
+            item["author"] = item["author"][3:]
+        
+        collection.insert_one(dict(item))
+        return item
+
+class JsonWriterPipeline(object):
+    def open_spider(self, spider):
+        self.file = open('items.json', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
+        return item
