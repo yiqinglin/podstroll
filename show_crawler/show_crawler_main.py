cf59db13b3811adac2d08b14a261b9fd6f1f597f
Yiqing <yiqinglin.me@gmail.com>
2019-03-17 19:42:15 -0700

    Refactor show crawler and add sub commands.

--- show_crawler/show_crawler_main.py
+++ show_crawler/show_crawler_main.py
@@ -1,96 +1,141 @@
-from pymongo import MongoClient
-import requests
+from __future__ import print_function
+
+import argparse
+import logging
+from multiprocessing import Pool
+
 import feedparser
+import requests
+from pymongo import MongoClient
+
 import settings
 
 
-def main():
-    print ('Updating podcast feed url...')
-    # self.mongo_uri = settings.MONGO_URI
-    # self.mongo_db = settings.MONGO_DATABASE
-    # self.mongo_collection = settings.MONGO_COLLECTION
+def crawl_feed_url(args):
+    crawler = ShowCrawler(
+        settings.MONGO_URI,
+        settings.MONGO_DATABASE,
+        settings.MONGO_COLLECTION,
+    )
+    crawler.update_feed_url()
+    logging.info('All done!')
 
-    client = MongoClient(settings.MONGO_URI)
-    db = client[settings.MONGO_DATABASE]
-    collection = db[settings.MONGO_COLLECTION]
-    podcasts = collection.find({})
 
-    for pod in podcasts:
-        podcast_id = pod['podcast_id']
-        url = settings.LOOK_UP_BASE + podcast_id
-        else:
+def crawl_metadata(args):
+    crawler = ShowCrawler(
+        settings.MONGO_URI,
+        settings.MONGO_DATABASE,
+        settings.MONGO_COLLECTION,
+    )
+    crawler.update_metadata()
+    logging.info('All done!')
+
+
+class ShowCrawler(object):
+    def __init__(self, mongo_uri, database, collection):
+        self.client = MongoClient(mongo_uri)
+        self.db = self.client[database]
+        self.collection = self.db[collection]
+        self.podcasts = self.collection.find({})
+
+    def update_feed_url(self):
+        logger = logging.getLogger('feed_url')
+        logger.info('Updating podcast feed url...')
+
+        for pod in self.podcasts:
+            podcast_id = pod['podcast_id']
+            url = settings.LOOK_UP_BASE + podcast_id
+
             try:
                 r = requests.get(url)
                 data = r.json()
                 feed_url = data['results'][0].get('feedUrl', None)
-                
+
                 # If podcast has no feedUrl, remove it from the db.
                 if not feed_url:
-                    print ('removing the podcast as it does not have feed url:', podcast_id)
-                    collection.delete_one({'_id': pod['_id']})
+                    logger.warning(
+                        'Removing the podcast as it does not have feed url: %s',
+                        podcast_id)
+                    self.collection.delete_one({'_id': pod['_id']})
                     continue
 
                 # If the feed url returns 404, remove it from the db.
                 if requests.get(feed_url).status_code == 404:
-                    print ('removing the podcast as it does not have a valid feed url:', podcast_id)
-                    collection.delete_one({'_id': pod['_id']})
+                    logger.warning(
+                        'Removing the podcast as it does not have a valid feed url: %s',
+                        podcast_id)
+                    self.collection.delete_one({'_id': pod['_id']})
                     continue
 
-                print ('updating feedUrl to podcast', pod['podcast_id'])
-                collection.find_one_and_update({'_id': pod['_id']}, {'$set': {'feed_url': feed_url}})
+                # If the feed url remains the same, continue to the next podcast.
+                if feed_url == pod.get('feed_url', None):
+                    continue
+
+                logger.info('Updating feedUrl to podcast %s',
+                            pod['podcast_id'])
+                self.collection.find_one_and_update(
+                    {'_id': pod['_id']}, {'$set': {
+                        'feed_url': feed_url
+                    }})
             except Exception as error:
-                print ('Caught this error:', repr(error))
+                logger.exception(error)
 
-        print ("All done!")
+    def update_metadata(self):
+        logger = logging.getLogger('metadata')
+        logger.info('Updating podcast details using feed url...')
 
+        for pod in self.podcasts:
+            feed_url = pod['feed_url']
+            logger.debug(feed_url)
+            d = feedparser.parse(feed_url)
+            pod_channel = d['channel']
 
-def updatePodcastDetails():
-    print ('Updating podcast details using feed url...')
+            website_link = pod_channel.get('link', '')
+            description = pod_channel.get('description', '')
+            summary = pod_channel.get('summary', '')
 
-    client = MongoClient(settings.MONGO_URI)
-    db = client[settings.MONGO_DATABASE]
-    collection = db[settings.MONGO_COLLECTION]
-    podcasts = collection.find({})
+            pod_img = pod_channel.get('image', None)
+            image = pod_img.get('url', '') if pod_img else ''
+            pod_type = pod_channel.get('itunes_type', '')
+            subtitle = pod_channel.get('subtitle', '')
+            itunes_explicit = pod_channel.get('itunes_explicit', '')
+            episode_count = len(d.entries)
 
-    for pod in podcasts:
-        feed_url = pod['feed_url']
-        print (feed_url)
-        d = feedparser.parse(feed_url)
-        pod_channel = d['channel']
-        
+            logger.info(
+                'Writing into database with detailed information for podcast %s',
+                pod['podcast_id'])
 
-        website_link = pod_channel.get('link', '')
-        description = pod_channel.get('description', '')
-        summary = pod_channel.get('summary', '')
-
-        pod_img = pod_channel.get('image', None)
-        image = pod_img.get('url', '') if pod_img else ''
-        pod_type = pod_channel.get('itunes_type', '')
-        subtitle = pod_channel.get('subtitle', '')
-        itunes_explicit = pod_channel.get('itunes_explicit', '')
-        episode_count = len(d.entries)
-
-        print ('Writing into database with detailed information for podcast', pod['podcast_id'])
-
-        collection.find_one_and_update({'_id': pod['_id']}, {'$set': {
-            'subtitle': subtitle,
-            'website_link': website_link,
-            'description': description,
-            'summary': summary,
-            'pod_type': pod_type,
-            'episode_count': episode_count,
-            'itunes_explicit': itunes_explicit,
-            'image': image
-        }})
-
-    print ("All done!")
-    
-
-        
-        
+            self.collection.find_one_and_update({'_id': pod['_id']}, {
+                '$set': {
+                    'subtitle': subtitle,
+                    'website_link': website_link,
+                    'description': description,
+                    'summary': summary,
+                    'pod_type': pod_type,
+                    'episode_count': episode_count,
+                    'itunes_explicit': itunes_explicit,
+                    'image': image
+                }
+            })
 
 
-        
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
 
+    # Create the top-level parser
+    parser = argparse.ArgumentParser(prog='show_crawler')
+    subparsers = parser.add_subparsers(help='sub-command help')
 
+    # Create the parser for the "feedurl" command
+    parser_feedurl = subparsers.add_parser(
+        'feed', help="Crawl shows' feed urls.")
+    parser_feedurl.set_defaults(func=crawl_feed_url)
 
+    # Create the parser for the "metadata" command
+    parser_metadata = subparsers.add_parser(
+        'metadata', help="Crawl shows' metadata.")
+    parser_metadata.set_defaults(func=crawl_metadata)
+
+    # Parse the args and call whatever function was selected
+    args = parser.parse_args()
+    args.func(args)
